c5453953b122922e2dee85106d5c200456be6925
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-05-02 16:17:43 +0200

    initial commit

---
+++ cli_first/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    
+    <FirstTitle/>
+    <HeroImg/>
+
+  </div>
+</template>
+
+<script>
+import FirstTitle from './components/FirstTitle.vue';
+import HeroImg from './components/HeroImg.vue';
+
+export default {
+  name: 'App',
+  components: {
+    FirstTitle,
+    HeroImg
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ cli_first/src/components/HeroImg.vue
@@ -0,0 +1,9 @@
+<template>
+    <img src="../assets/logo.png" alt="">
+</template>
+
+<script>
+export default {
+    name: 'HeroImg'
+} 
+</script>
